e37bb3df19f4077ab77f84fb717e37316ed74097
ititiu20312quocthinh <91869213+ititiu20312quocthinh@users.noreply.github.com>
2021-11-22 14:37:36 +0700

    exe1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -69,8 +69,7 @@ void Ex1(int n){
   case 2:printf(" two \n"); break;
   case 3:printf(" three \n"); break;
   case 4:printf(" four \n"); break;
-  case 5:if (a[1] == 0) printf(" five \n");
-      else printf(" five \n"); break;
+  case 5:printf(" five \n"); break;
   case 6:printf(" six \n"); break;
   case 7:printf(" seven \n"); break;
   case 8:printf(" eight \n"); break;
